7adffa4c3a29d252c7e7aea0e864870fbc64a276
happinessncube <happinessmncube@gmail.com>
2023-01-03 21:12:07 +0200

    Fix processData not to include today's details

--- app/src/main/java/com/hmncube/myweather/ui/forecast_weather/ForecastWeatherViewModel.kt
+++ app/src/main/java/com/hmncube/myweather/ui/forecast_weather/ForecastWeatherViewModel.kt
@@ -1,10 +1,12 @@
 package com.hmncube.myweather.ui.forecast_weather
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hmncube.myweather.data.WeatherRepository
 import com.hmncube.myweather.data.database.WeatherData
+import com.hmncube.myweather.data.remote.models.WeatherValues
 import com.hmncube.myweather.ui.utils.ImagesUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.util.*
@@ -34,11 +36,18 @@ class ForecastWeatherViewModel @Inject constructor(private val repository: Weath
 
     private fun processData(weatherData: WeatherData): List<DailyForecastData> {
         val forecasts = mutableListOf<DailyForecastData>()
-        var lastDate = Calendar.getInstance()
+
         var max = Double.MIN_VALUE
         var min = Double.MAX_VALUE
+        var weatherIcons = mutableListOf<String>()
         var weatherDescriptions = mutableListOf<String>()
-        for (forecast in weatherData.forecasts) {
+        var humidityList = mutableListOf<Int>()
+        var windSpeedList = mutableListOf<Double>()
+        var visibilityList = mutableListOf<Int>()
+
+        val weatherForecasts = removeCurrentDayWeather(weatherData.forecasts)
+        var lastDate = Date(weatherForecasts[0].dt?.toLong()!! * 1000).toCalendar()
+        for (forecast in weatherForecasts) {
             val date = Date(forecast.dt?.toLong()!! * 1000).toCalendar()
             if (date.get(Calendar.DAY_OF_MONTH) == lastDate.get(Calendar.DAY_OF_MONTH)) {
                 val temp = forecast.main?.temp!!
@@ -47,32 +56,84 @@ class ForecastWeatherViewModel @Inject constructor(private val repository: Weath
                 } else if (temp < min) {
                     min = temp
                 }
-                weatherDescriptions.add(forecast.weather[0].icon!!)
+                weatherIcons.add(forecast.weather[0].icon!!)
+                weatherDescriptions.add(forecast.weather[0].description!!)
+                humidityList.add(forecast.main?.humidity!!)
+                windSpeedList.add(forecast.wind?.speed!!)
+                visibilityList.add(forecast.visibility!!)
             } else {
-                lastDate = date
-                val weatherIcon = getWeatherIconFromWeatherDescriptions(weatherDescriptions)
+                val weatherIcon = getFrequentElement(weatherIcons)
+                weatherIcons = mutableListOf()
+
+                val description = getFrequentElement(weatherDescriptions)
                 weatherDescriptions = mutableListOf()
+
+                val averageHumidity = calculateHumidityAverage(humidityList)
+                humidityList = mutableListOf()
+
+                val averageWindSpeed = calculateWindSpeedAverage(windSpeedList)
+                windSpeedList = mutableListOf()
+
+                val averageVisibility = calculateHumidityAverage(visibilityList) / 1000
+                visibilityList = mutableListOf()
+
                 forecasts.add(
                     DailyForecastData(
                         max.roundToInt(),
                         min.roundToInt(),
-                        date.time,
-                        forecast.weather[0].description!!,
-                        forecast.wind?.speed!!,
-                        forecast.main?.humidity!!,
-                        forecast.visibility!! / 1000,
+                        lastDate.time,
+                        description,
+                        averageWindSpeed,
+                        averageHumidity,
+                        averageVisibility,
                         ImagesUtils.getAnimationFromWeatherCode(weatherIcon)
                     )
                 )
                 max = Double.MIN_VALUE
                 min = Double.MAX_VALUE
+                lastDate = date
             }
         }
         return forecasts
     }
 
-    private fun getWeatherIconFromWeatherDescriptions(weatherDescriptions: MutableList<String>): String {
-        val descriptionCounts = weatherDescriptions.groupingBy { it }.eachCount()
+    private fun calculateHumidityAverage(list: MutableList<Int>): Int {
+        return list.sum() / list.size
+    }
+
+    private fun calculateWindSpeedAverage(windSpeedList: MutableList<Double>): Double {
+        return windSpeedList.sum() / windSpeedList.size
+    }
+
+    private fun removeCurrentDayWeather(forecasts: List<WeatherValues>): List<WeatherValues> {
+        val values = mutableListOf<WeatherValues>()
+        val todayDate = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+
+        for (forecast in forecasts) {
+            val date = Date(forecast.dt?.toLong()!! * 1000).toCalendar()
+            val day = date.get(Calendar.DAY_OF_MONTH)
+            if (todayDate != day) {
+                values.add(forecast)
+            }
+        }
+        return values
+    }
+
+    private fun isNotToday(date: Calendar): Boolean {
+        val month = date.get(Calendar.MONTH)
+        val day = date.get(Calendar.DAY_OF_MONTH)
+
+        val thisMonth = Calendar.getInstance().get(Calendar.MONTH)
+        val thisDate = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+
+        if (thisMonth == month) {
+            return day != thisDate
+        }
+        return true
+    }
+
+    private fun getFrequentElement(data: MutableList<String>): String {
+        val descriptionCounts = data.groupingBy { it }.eachCount()
         return descriptionCounts.maxBy { it.value }.key
     }
 }
